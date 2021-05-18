d471cf602a1d0e09cbebda3cbd2549d3e9646adc
nguyenvongoc <65283435+nguyenvongoc@users.noreply.github.com>
2021-05-18 19:37:40 +0700

    baitap

---
+++ tinh_tong/laisuat.js
@@ -0,0 +1,5 @@
+let stvay = +prompt("Nhap so tien vay = ");
+let lsuat = +prompt("Nhap lai suat cua ngan hang = ");
+let sn = +prompt("Nhap so nam vay = ");
+sum= stvay + (lsuat*sn);
+alert("So tien phai tra sau "+sn+" nam = "+sum);
---
+++ tinh_tong/my.js
@@ -0,0 +1,4 @@
+let a =+prompt("Nhap a = ");
+let b =+prompt("Nhap b = ");
+sum= a+b;
+alert("Tong 2 so "+a+" + "+b+" = " +sum);
