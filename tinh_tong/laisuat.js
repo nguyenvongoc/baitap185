0f0b1cd2734b6aae66b1058b29cda73c9ef0fd31
nguyenvongoc <65283435+nguyenvongoc@users.noreply.github.com>
2021-05-18 20:14:28 +0700

    baitap185

--- tinh_tong/laisuat.js
+++ tinh_tong/laisuat.js
@@ -1,5 +1,5 @@
 let stvay = +prompt("Nhap so tien vay = ");
 let lsuat = +prompt("Nhap lai suat cua ngan hang = ");
 let sn = +prompt("Nhap so nam vay = ");
-sum= stvay + (lsuat*sn);
+sum= stvay + stvay*(lsuat/100)*sn;
 document.write("So tien phai tra sau "+sn+" nam = "+sum);
---
+++ tinh_tong/tron.js
@@ -0,0 +1,6 @@
+let r=+prompt("Nhap ban kinh hinh tron : ");
+const pi=3.14;
+c=2*r*pi;
+s=r*r*pi;
+document.writeln("Chu vi hinh tron = "+c+"<br>");
+document.writeln("Dien tich hinh tron = "+s);
